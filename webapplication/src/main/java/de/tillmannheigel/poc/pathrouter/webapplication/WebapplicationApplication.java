5d729e4d65ab8e18d0502c7c65354cc8b91e3983
Tillmann Heigel <tillmann.heigel@idealo.de>
2018-03-27 22:15:47 +0200

    registry: Initialize application.yml with some eureka settings.

--- webapplication/src/main/java/de/tillmannheigel/poc/pathrouter/webapplication/WebapplicationApplication.java
+++ webapplication/src/main/java/de/tillmannheigel/poc/pathrouter/webapplication/WebapplicationApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebapplicationApplication {
 
-    @GetMapping("/")
+    @GetMapping({ "/", "/mypath" })
     public String getHome() {
         return "Moin";
     }
