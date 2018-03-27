36b79d56bba0773d3b5bf81002aa1e5297d450bc
Tillmann Heigel <tillmann.heigel@idealo.de>
2018-03-27 20:36:41 +0200

    spring-initialzr: ribbon-lb added

---
+++ loadbalancer/src/main/java/de/tillmannheigel/poc/pathrouter/loadbalancer/LoadbalancerApplication.java
@@ -0,0 +1,12 @@
+package de.tillmannheigel.poc.pathrouter.loadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadbalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadbalancerApplication.class, args);
+	}
+}
