35e1b802580818bb0fc84850f18f68a6261ccca0
Tillmann Heigel <tillmann.heigel@idealo.de>
2018-03-27 21:34:35 +0200

    webapplication: added spring cloud version to pom and spring cloud starter eureka

--- webapplication/src/main/java/de/tillmannheigel/poc/pathrouter/webapplication/WebapplicationApplication.java
+++ webapplication/src/main/java/de/tillmannheigel/poc/pathrouter/webapplication/WebapplicationApplication.java
@@ -2,17 +2,21 @@ package de.tillmannheigel.poc.pathrouter.webapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
 public class WebapplicationApplication {
 
-	@RequestMapping("/")
-	public String getPage() {
-		return "Moin";
-	}
+    @GetMapping("/")
+    public String getHome() {
+        return "Moin";
+    }
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebapplicationApplication.class, args);
