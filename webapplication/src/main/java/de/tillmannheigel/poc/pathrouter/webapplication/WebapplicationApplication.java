90f37c17de8354cde2b6b8717c2ae4a305e5a8c0
Tillmann Heigel <tillmann.heigel@idealo.de>
2018-03-27 21:08:12 +0200

    webapplication: added simple request mapping for /

--- webapplication/src/main/java/de/tillmannheigel/poc/pathrouter/webapplication/WebapplicationApplication.java
+++ webapplication/src/main/java/de/tillmannheigel/poc/pathrouter/webapplication/WebapplicationApplication.java
@@ -2,9 +2,17 @@ package de.tillmannheigel.poc.pathrouter.webapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WebapplicationApplication {
+
+	@RequestMapping("/")
+	public void getPage(){
+		return "Moin";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebapplicationApplication.class, args);
