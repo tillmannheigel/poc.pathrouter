0f82a5d12745146950f266eec2b82414cbd3c704
Tillmann Heigel <tillmann.heigel@idealo.de>
2018-03-27 22:52:12 +0200

    router: enable zuul proxy

--- router/src/main/java/de/tillmannheigel/poc/pathrouter/router/RouterApplication.java
+++ router/src/main/java/de/tillmannheigel/poc/pathrouter/router/RouterApplication.java
@@ -2,8 +2,10 @@ package de.tillmannheigel.poc.pathrouter.router;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class RouterApplication {
 
 	public static void main(String[] args) {
