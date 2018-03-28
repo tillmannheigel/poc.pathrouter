f24eacbe5bd94868dea340b9025179b7fcb3b6ba
Tillmann Heigel <tillmann.heigel@idealo.de>
2018-03-28 09:30:15 +0200

    router: register ribbon at eureka as client

--- router/src/main/java/de/tillmannheigel/poc/pathrouter/router/RouterApplication.java
+++ router/src/main/java/de/tillmannheigel/poc/pathrouter/router/RouterApplication.java
@@ -2,10 +2,12 @@ package de.tillmannheigel.poc.pathrouter.router;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class RouterApplication {
 
 	public static void main(String[] args) {
