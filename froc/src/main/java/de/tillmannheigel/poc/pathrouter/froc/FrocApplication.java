2cb5ab01b78728ad3c0189decb26990f9b2c795b
Tillmann Heigel <tillmann.heigel@idealo.de>
2018-05-25 17:23:55 +0200

    froc: initial commit

---
+++ froc/src/main/java/de/tillmannheigel/poc/pathrouter/froc/FrocApplication.java
@@ -0,0 +1,12 @@
+package de.tillmannheigel.poc.pathrouter.froc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrocApplication.class, args);
+    }
+}
---
+++ froc/src/test/java/de/tillmannheigel/poc/pathrouter/froc/FrocApplicationTests.java
@@ -0,0 +1,16 @@
+package de.tillmannheigel.poc.pathrouter.froc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrocApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
